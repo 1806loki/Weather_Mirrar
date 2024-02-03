d227e384edd281f7f2b5c8fada4d93e562f99a73
1806loki <127595726+1806loki@users.noreply.github.com>
2024-02-03 16:42:47 +0530

    Add Background Images Api

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,16 +10,16 @@ const HomePage = () => {
   const [enteredPlace, setEnteredPlace] = useState("");
   const [weatherData, setWeatherData] = useState({});
   const [forecastData, setForecastData] = useState({});
-  // const [backgroundImage, setBackgroundImage] = useState(null);
+  const [backgroundImage, setBackgroundImage] = useState(null);
   const [temperatureUnit, setTemperatureUnit] = useState("metric");
-  // const [weatherCondition, setWeatherCondition] = useState("");
+  const [weatherCondition, setWeatherCondition] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await fetchCurrentWeather(enteredPlace, temperatureUnit);
         setWeatherData(data);
-        // setWeatherCondition(data?.weather?.[0]?.main);
+        setWeatherCondition(data?.weather?.[0]?.main);
       } catch (error) {
         console.log(error);
       }
@@ -45,28 +45,28 @@ const HomePage = () => {
     }
   }, [enteredPlace, temperatureUnit]);
 
-  // useEffect(() => {
-  //   const fetchImage = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         `https://api.unsplash.com/photos/random?query=${weatherCondition}&orientation=landscape&featured=true&client_id=Gn3_6ZhUE9LQ_w_xLh0Pmh2rXvDQvMsaMbHm7f_HrC4`
-  //       );
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await fetch(
+          `https://api.unsplash.com/photos/random?query=${weatherCondition}&orientation=landscape&featured=true&client_id=Gn3_6ZhUE9LQ_w_xLh0Pmh2rXvDQvMsaMbHm7f_HrC4`
+        );
 
-  //       if (!response.ok) {
-  //         throw new Error(`Failed to fetch image: ${response.statusText}`);
-  //       }
+        if (!response.ok) {
+          throw new Error(`Failed to fetch image: ${response.statusText}`);
+        }
 
-  //       const imageData = await response.json();
-  //       setBackgroundImage(imageData.urls.regular);
-  //     } catch (error) {
-  //       console.error("Error fetching image:", error);
-  //     }
-  //   };
+        const imageData = await response.json();
+        setBackgroundImage(imageData.urls.regular);
+      } catch (error) {
+        console.error("Error fetching image:", error);
+      }
+    };
 
-  //   if (enteredPlace) {
-  //     fetchImage();
-  //   }
-  // }, [weatherCondition]);
+    if (enteredPlace) {
+      fetchImage();
+    }
+  }, [weatherCondition]);
 
   const handlePlaceChange = (place) => {
     setEnteredPlace(place);
@@ -78,7 +78,7 @@ const HomePage = () => {
   return (
     <div
       className="HomePage"
-      // style={{ backgroundImage: `url(${backgroundImage})` }}
+      style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <SearchBar onPlaceChange={handlePlaceChange} data={weatherData} />
       <div className="container">
